0eb2046ad1562a51f3ed3fe70d967126a9b4d7dc
krisvc <kpvonczarnowski@oakland.edu>
2023-04-14 16:40:38 -0700

    April14th_push_2

--- audibot_acc/src/audibot_acc.cpp
+++ audibot_acc/src/audibot_acc.cpp
@@ -5,10 +5,12 @@
 #include <geometry_msgs/Twist.h> // Library for linear and angular velocity for x, y, and z axes.// Implementing header files in the program
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/TwistStamped.h>
+#include <sensor_msgs/LaserScan.h>
+#include <algorithm>
 
 // Program to implement adaptive cruise control ACC feature
-// Version 1.1 
-// Date: 4/2/2023
+// Version 1.2 
+// Date: 4/14/2023
 // Cleaned up code/comments and fixed errors in previous version
 // Authors (github names):
 // jseablom (jseablom)
@@ -26,6 +28,12 @@
 
 // Global variables to store the position of the target and ego vehicle models
 
+int stage = 1;
+//set 'stage' variable to:
+// 0 - Level 1: Implement core system with ideal measurements
+// 1 - Level 2: Use a LIDAR sensor to detect the lead vehicle
+// 2 - Level 3: Use the camera to detect the lead vehicle
+
 geometry_msgs::Pose target_vehicle_position;
 geometry_msgs::Pose ego_vehicle_position; 
 geometry_msgs::Twist twist_cmd;
@@ -36,6 +44,12 @@ double twist_cmd_placeholder = 0;
 
 //Create static publisher
 static ros::Publisher pub; 
+static ros::Subscriber sub_laser;
+
+double displacement = 100;
+
+// Main function - Initializing the adaptive cruise control (ACC) node, subscribers and timers.
+
 
 // Callback function whenever a new /gazebo/model_states message is received
 void modelStatesCallbackFunction(const gazebo_msgs::ModelStates msg) {
@@ -61,6 +75,37 @@ void modelStatesCallbackFunction(const gazebo_msgs::ModelStates msg) {
 
 }
 
+// Callback function whenever a new lidar scan message is received
+void lidarCallbackFunction(const sensor_msgs::LaserScan::ConstPtr& msg) {
+/*Header header            # timestamp in the header is the acquisition time of 
+                         # the first ray in the scan.
+                         #
+                         # in frame frame_id, angles are measured around 
+                         # the positive Z axis (counterclockwise, if Z is up)
+                         # with zero angle being forward along the x axis
+                         
+float32 angle_min        # start angle of the scan [rad]
+float32 angle_max        # end angle of the scan [rad]
+float32 angle_increment  # angular distance between measurements [rad]
+
+float32 time_increment   # time between measurements [seconds] - if your scanner
+                         # is moving, this will be used in interpolating position
+                         # of 3d points
+float32 scan_time        # time between scans [seconds]
+
+float32 range_min        # minimum range value [m]
+float32 range_max        # maximum range value [m]
+
+float32[] ranges         # range data [m] (Note: values < range_min or > range_max should be discarded)
+float32[] intensities    # intensity data [device-specific units].  If your
+                         # device does not provide intensities, please leave
+                         # the array empty.*/
+  displacement = *std::min_element(msg->ranges.begin(), msg->ranges.end());
+  ROS_INFO("Lidar message received...");
+
+
+}
+
 void twistTime(const geometry_msgs::TwistStamped msg) {
   twist_cmd_placeholder = msg.twist.angular.z;
 }
@@ -68,6 +113,7 @@ void twistTime(const geometry_msgs::TwistStamped msg) {
 // Timer callback - runs every 100ms
 void timerCallback(const ros::TimerEvent& event) 
 {
+  if (stage == 0) {
   // Calculate x, y, and z displacement in cartesian coordinates and find geometric mean 
   double dx = target_vehicle_position.position.x - ego_vehicle_position.position.x;
   double dy = target_vehicle_position.position.y - ego_vehicle_position.position.y;
@@ -76,13 +122,23 @@ void timerCallback(const ros::TimerEvent& event)
   // The displacement calculatation is assuming both vehicles traveling in a straight line!
   // This is not true when the vehicles switch lanes or take a curve
 
-  double displacement  = std::sqrt(dx*dx + dy*dy + dz*dz);
+  displacement = std::sqrt(dx*dx + dy*dy + dz*dz);
+  }
+
+  if (stage == 1) {
+
+    if (displacement == std::numeric_limits<double>::infinity()) {
+      ROS_INFO("Waiting for LIDAR scan message to be received...");
+    return;
+  }
+  
+}
 
   // Print calculated displacement to the console for debugging purposes
   ROS_INFO("Following distance = %f meters", displacement);
 
   // Control algorithm for linear speed based on straight-line displacement and defined following distance
-  double linear_speed = 0.0; 
+  double linear_speed = 30.0; 
 
   // Try to get parameter "speed" from launch file
   if (!ros::param::get("speed", linear_speed)) {
@@ -91,6 +147,7 @@ void timerCallback(const ros::TimerEvent& event)
   }
 
   ROS_INFO("Steering angle = %f radians", twist_cmd_placeholder);
+  ROS_INFO("Stage = %f + 1", stage);
 
   // Basic control algorithm, may want to implement PID controller, as Yaswanth was suggesting...
   // We may also want to control our speed based on time-to-collision (or 'TTC') or relative velocity.
@@ -123,15 +180,30 @@ void timerCallback(const ros::TimerEvent& event)
 
 }
 
-// Main function - Initializing the adaptive cruise control (ACC) node, subscribers and timers.
 int main(int argc, char **argv) {
   ros::init(argc, argv, "audibot_acc");
+
+  if (stage == 1) { 
+  double displacement = std::numeric_limits<double>::infinity(); // initialize to infinity
+  }
+
+  if (stage == 0) {
+  double displacement = 0;
+  }
 
   // Ceate a nodehandle for the acc node
   ros::NodeHandle node_handle;
 
+  if (stage == 0) {
   // Subscriber to "gazebo/model_states"
   ros::Subscriber model_states_subscriber = node_handle.subscribe("/gazebo/model_states", 1, modelStatesCallbackFunction);
+  }
+
+  if (stage == 1) {
+  // Subscriber to "/ego_vehicle/laser/scan"
+  ros::Subscriber lidar_subscriber = node_handle.subscribe("/ego_vehicle/laser/scan", 1, lidarCallbackFunction);  
+  ROS_INFO("Subscribed to stage 1...");
+  }
 
   ros::Subscriber twist_subscriber = node_handle.subscribe("/ego_vehicle/twist", 1, twistTime);
 
